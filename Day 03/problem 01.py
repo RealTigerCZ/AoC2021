5ea1741c0768d9b5b4dc74428decd67d6126f4e8
RealTigerCZ <hucellj@seznam.cz>
2021-12-19 10:45:08 +0100

    Solve Day 03

--- Day 03/problem 01.py
+++ Day 03/problem 01.py
@@ -3,4 +3,33 @@ path = "input.txt"
 file = open(path)
 input = file.readlines()
 file.close()
- 
+
+lenght = len(input[0][:-1])
+
+zeros = [0 for i in range(lenght)]
+ones = [0 for i in range(lenght)]
+
+for line in input:
+    for idx, item in enumerate(line):
+        if item == "0":
+            zeros[idx] += 1
+        elif item == "1":
+            ones[idx] += 1
+
+
+gamma_rate = []
+epsilon = []
+
+for idx in range(lenght):
+    if zeros[idx] > ones[idx]:
+        gamma_rate.append("0")
+        epsilon.append("1")
+    else:
+        gamma_rate.append("1")
+        epsilon.append("0")
+
+gamma_rate = "".join(gamma_rate)
+epsilon = "".join(epsilon)
+
+print(f"Gamma rate: ({gamma_rate} : {int(gamma_rate, 2)}), epsilon ({epsilon} : {int(epsilon, 2)})")
+print(f"Result: {int(gamma_rate, 2)*int(epsilon, 2)}")
--- Day 03/problem 02.py
+++ Day 03/problem 02.py
@@ -3,3 +3,51 @@ path = "input.txt"
 file = open(path)
 input = file.readlines()
 file.close()
+
+for i in range(len(input)):
+    input[i] = input[i][:-1]
+
+
+oxigen = input[:]
+
+bit = 0
+while len(oxigen) > 1:
+    zeros, ones = 0, 0
+    for item in oxigen:
+        if item[bit] == "0":
+            zeros += 1
+        elif item[bit] == "1":
+            ones += 1
+
+    char = "1" if ones >= zeros else "0"
+    i = 0
+    while i < len(oxigen):
+        if oxigen[i][bit] != char:
+            oxigen.pop(i)
+        else:
+            i += 1
+    bit += 1
+
+CO2 = input[:]
+
+bit = 0
+while len(CO2) > 1:
+    zeros, ones = 0, 0
+    for item in CO2:
+        if item[bit] == "0":
+            zeros += 1
+        elif item[bit] == "1":
+            ones += 1
+
+    char = "0" if zeros <= ones else "1"
+    i = 0
+    while i < len(CO2):
+        if CO2[i][bit] != char:
+            CO2.pop(i)
+        else:
+            i += 1
+
+    bit += 1
+
+print(oxigen, CO2)
+print("Result: ", int(oxigen[0], 2) * int(CO2[0], 2))
