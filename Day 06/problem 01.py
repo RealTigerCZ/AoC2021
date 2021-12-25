5ecfff1c15f75a1d2107436174ff231500754ce4
RealTigerCZ <hucellj@seznam.cz>
2021-12-25 12:32:54 +0100

    Solved Problem 01 of Day 6

--- Day 06/problem 01.py
+++ Day 06/problem 01.py
@@ -3,3 +3,17 @@ path = "input.txt"
 file = open(path)
 input = file.readlines()
 file.close()
+
+all_fish = [[int(x) for x in line.split(",")] for line in input][0]
+
+for i in range(80):
+    new = 0
+    for idx in range(len(all_fish)):
+        if all_fish[idx] == 0:
+            all_fish[idx] = 6
+            new += 1
+        else:
+            all_fish[idx] -= 1
+    all_fish += [8 for _ in range(new)]
+
+print(len(all_fish))
