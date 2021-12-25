a3ad6306e481f0180693588ddd18a2b026a6842c
RealTigerCZ <hucellj@seznam.cz>
2021-12-25 12:40:02 +0100

    Solved Problem 02 of Day 6

--- Day 06/problem 02.py
+++ Day 06/problem 02.py
@@ -3,3 +3,18 @@ path = "input.txt"
 file = open(path)
 input = file.readlines()
 file.close()
+
+all_fish = [[int(x) for x in line.split(",")] for line in input][0]
+
+fish_count_by_index = [0 for _ in range(9)]
+
+for fish in all_fish:
+    fish_count_by_index[fish] += 1
+
+for day in range(256):
+    zeros = fish_count_by_index.pop(0)
+    fish_count_by_index.append(zeros)
+    fish_count_by_index[6] += zeros
+
+
+print(sum(fish_count_by_index))
