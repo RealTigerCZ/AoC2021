302b8f922f36e5e1d149194d88807f1ae0a91dbb
RealTigerCZ <hucellj@seznam.cz>
2021-12-27 11:23:31 +0100

    Solved problem 01 of Day 7

--- Day 07/problem 01.py
+++ Day 07/problem 01.py
@@ -3,3 +3,11 @@ path = "input.txt"
 file = open(path)
 input = file.readlines()
 file.close()
+
+coords = sorted([[int(n) for n in line.split(",")] for line in input][0])
+
+# for i in range(min(coords), max(coords) + 1):
+#     print(f"{i}:", sum([abs(n-i) for n in coords]))
+
+i = coords[len(coords)//2]
+print(f"{i}:", sum([abs(n-i) for n in coords]))
