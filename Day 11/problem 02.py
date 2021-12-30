db8f94386e288dc6a7e0198017294d50302c16f1
RealTigerCZ <hucellj@seznam.cz>
2021-12-30 15:43:40 +0100

    Solved Day 11

--- Day 11/problem 02.py
+++ Day 11/problem 02.py
@@ -3,3 +3,60 @@ path = "input.txt"
 file = open(path)
 input = [line[:-1] for line in file.readlines()]
 file.close()
+
+class Octopus:
+    def __init__(self, x, y, energy):
+        self.x = x
+        self.y = y
+        self.energy = energy
+        self.neighbours = []
+        self.flashed = False
+
+    def find_neighbours(self, octo):
+        dirs = [(1,0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
+        for dir in dirs:
+            x = dir[0] + self.x
+            y = dir[1] + self.y
+            if x < X_MAX and x >= 0 and y < Y_MAX and y >= 0:
+                self.neighbours.append(octo[y][x])
+
+    def flash(self):
+        self.energy += 1
+        if self.energy > 9:
+            self.energy = 0
+            self.flashed = True
+            for n in self.neighbours:
+                if not n.flashed:
+                    n.flash()
+
+
+Octopuses = [[Octopus(x, y, int(n)) for x, n in enumerate(line)] for y, line in enumerate(input)]
+X_MAX = len(Octopuses[0])
+Y_MAX = len(Octopuses)
+
+for line in Octopuses:
+    for o in line:
+        o.find_neighbours(Octopuses)
+
+
+sync = False
+gen = 0
+while not sync:
+    gen += 1
+    for line in Octopuses:
+        for o in line:
+            if not o.flashed:
+                o.flash()
+
+    for line in Octopuses:
+        for o in line:
+                o.flashed = False
+
+    e = Octopuses[0][0].energy
+    for line in Octopuses:
+        for o in line:
+            if e != o.energy:
+                sync = False
+                break
+            sync = True
+print(gen)
